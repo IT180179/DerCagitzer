83ae023b418f66bf18adbb98f8a8e11fb83b3cc4
David Ursprung <59611676+DavidUrsprung@users.noreply.github.com>
2022-01-26 17:14:01 +0100

    Table und Room Entities erstellt

---
+++ JPA/code-with-quarkus/src/main/java/org/acme/workloads/Room/Room.java
@@ -0,0 +1,39 @@
+package org.acme.workloads.Room;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Room {
+
+    @Id
+    @GeneratedValue
+    private Long room_id;
+    private String room_name;
+    private String summer_winter;
+
+    public Long getRoom_id() {
+        return room_id;
+    }
+
+    public void setRoom_id(Long room_id) {
+        this.room_id = room_id;
+    }
+
+    public String getRoom_name() {
+        return room_name;
+    }
+
+    public void setRoom_name(String room_name) {
+        this.room_name = room_name;
+    }
+
+    public String getSummer_winter() {
+        return summer_winter;
+    }
+
+    public void setSummer_winter(String summer_winter) {
+        this.summer_winter = summer_winter;
+    }
+}
---
+++ JPA/code-with-quarkus/src/main/java/org/acme/workloads/Table_Entity/Table_Entity.java
@@ -0,0 +1,41 @@
+package org.acme.workloads.Table_Entity;
+
+import org.acme.workloads.Room.Room;
+
+import javax.persistence.*;
+
+@Entity
+public class Table_Entity {
+
+    @Id
+    @GeneratedValue
+    private Long table_num;
+    private int seats;
+
+    @ManyToOne
+    private Room room_id;
+
+    public Long getTable_num() {
+        return table_num;
+    }
+
+    public void setTable_num(Long table_num) {
+        this.table_num = table_num;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public Room getRoom_id() {
+        return room_id;
+    }
+
+    public void setRoom_id(Room room_id) {
+        this.room_id = room_id;
+    }
+}
