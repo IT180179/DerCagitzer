b12a90f85f76389e4bd36d440e354d47862e9297
Annika Cagitz <annika.cagitzz@gmail.com>
2022-01-27 13:30:07 +0100

    Reservierung_Entity

---
+++ JPA/code-with-quarkus/src/main/java/org/acme/workloads/job/Job.java
@@ -0,0 +1,41 @@
+package org.acme.workloads.job;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer job_id;
+
+    private String job_title;
+    private boolean permission;
+
+    public Integer getJob_id() {
+        return job_id;
+    }
+
+    public void setJob_id(Integer job_id) {
+        this.job_id = job_id;
+    }
+
+    public String getJob_title() {
+        return job_title;
+    }
+
+    public void setJob_title(String job_title) {
+        this.job_title = job_title;
+    }
+
+    public boolean isPermission() {
+        return permission;
+    }
+
+    public void setPermission(boolean permission) {
+        this.permission = permission;
+    }
+}
