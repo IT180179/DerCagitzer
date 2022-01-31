2953386599c9e21d517169af478ddea3a250bc7c
IT180205 <56590851+IT180205@users.noreply.github.com>
2022-01-31 11:22:07 +0100

    Table Entity updated

---
+++ JPA/code-with-quarkus/src/main/java/org/acme/workloads/Customer/Customer.java
@@ -0,0 +1,45 @@
+package org.acme.workloads.Customer;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    private String first_name;
+
+    @Column(nullable = false)
+    private String last_name;
+    private String email;
+    private String password;
+    @Column(name = "telephoneNumber",nullable = false)
+    private String telenr;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+}
