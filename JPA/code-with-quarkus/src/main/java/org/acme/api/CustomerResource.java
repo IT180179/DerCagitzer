c16657a2e272e482b4001a2eafb338fa705d788c
David Ursprung <ursprungdavid@gmail.com>
2022-06-01 11:39:00 +0200

    Job Resource added

--- JPA/code-with-quarkus/src/main/java/org/acme/api/CustomerResource.java
+++ JPA/code-with-quarkus/src/main/java/org/acme/api/CustomerResource.java
@@ -45,5 +45,4 @@ public class CustomerResource {
             return Response.ok(customer).build();
     }
 
-
 }
--- JPA/code-with-quarkus/src/main/java/org/acme/api/JobResource.java
+++ JPA/code-with-quarkus/src/main/java/org/acme/api/JobResource.java
@@ -1,5 +1,46 @@
 package org.acme.api;
 
+import org.acme.model.CustomerDTO;
+import org.acme.model.JobDTO;
+import org.acme.workloads.Job.JobService;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Path("customer")
 public class JobResource {
 
+    private final JobService jobService;
+
+    public JobResource(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @GET
+    @Path("all")
+    public Response getAllJobs() {
+        var allJobs = this.jobService.getAllJobs();
+        return Response.ok(allJobs).build();
+    }
+
+    @POST
+    @Transactional
+    public Response addJob(JobDTO newJob){
+        var job = this.jobService.addJob(newJob);
+        if (job == null){
+            return Response.status(404).build();
+        }
+        return Response.ok(job).build();
+    }
+
+    @PUT
+    @Transactional
+    public Response update(JobDTO job) {
+        jobService.updateJob(job);
+        return Response.ok(job).build();
+    }
 }
