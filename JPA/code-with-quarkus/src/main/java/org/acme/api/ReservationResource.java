6af504a306a8b8ed17716e54780ddff6dd351494
David Ursprung <ursprungdavid@gmail.com>
2022-06-12 18:45:47 +0200

    Email

--- JPA/code-with-quarkus/src/main/java/org/acme/api/ReservationResource.java
+++ JPA/code-with-quarkus/src/main/java/org/acme/api/ReservationResource.java
@@ -1,22 +1,39 @@
 package org.acme.api;
 
+import io.smallrye.common.annotation.Blocking;
 import org.acme.model.ReservationDTO;
 import org.acme.workloads.Reservation.ReservationService;
 
+import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.Mailer;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 @Path("reservation")
 public class ReservationResource {
 
+    @Inject Mailer mailer;
+
     private final ReservationService reservationService;
 
     public ReservationResource(ReservationService reservationService) {
         this.reservationService = reservationService;
+    }
+
+    @GET
+    @Blocking
+    public void sendEmail() {
+        mailer.send(
+                Mail.withText("ursprungdavid@gmail.com",
+                        "Ahoy from Quarkus",
+                        "A simple email sent from a Quarkus application."
+                )
+        );
     }
 
     @POST
