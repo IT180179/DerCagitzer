e54f54ccd9b2890850c24bb941b84097beb9e4a0
IT180205 <56590851+IT180205@users.noreply.github.com>
2022-10-03 13:25:21 +0200

    created Timetable

--- Reservierungsystem/reservation-system/src/app/app.module.ts
+++ Reservierungsystem/reservation-system/src/app/app.module.ts
@@ -5,17 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DayviewPageComponent } from './site-components/dayview-page/dayview-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgWfTimetableModule} from "ng2-wf-timetable";
 
 @NgModule({
   declarations: [
     AppComponent,
     DayviewPageComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        NgWfTimetableModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- Reservierungsystem/reservation-system/src/app/site-components/dayview-page/dayview-page.component.ts
+++ Reservierungsystem/reservation-system/src/app/site-components/dayview-page/dayview-page.component.ts
@@ -1,5 +1,6 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {Timetable} from "../../shared/timetable";
+import {TimetableEvent, TimetableLocation, TimetableSchedule, TimetableScope} from "ng2-wf-timetable";
+
 
 @Component({
   selector: 'app-dayview-page',
@@ -8,17 +9,64 @@ import {Timetable} from "../../shared/timetable";
 })
 export class DayviewPageComponent implements OnInit {
 
+
+  scope!: TimetableScope;
+  schedules!: Array<TimetableSchedule>;
+
   constructor() {
-    this.timetable = new Timetable();
   }
 
-  @ViewChild('timetable') element: ElementRef;
-  timetable: Timetable;
-
-  locations = [];
-  events = [];
-
   ngOnInit(): void {
+    /*
+    The timetable scope which contain params in the following order:
+    1. Timetable start time: Date
+    2. Timetable end time: Date
+    3. (Optional) Number of pixels that should represent 5 minutes (32px by default): number
+    */
+    const timetableStartTime = new Date(new Date().setHours(8, 0, 0));
+    const timetableEndTime = new Date(new Date().setHours(16, 0, 0));
+
+    this.scope = new TimetableScope(timetableStartTime, timetableEndTime);
+
+    /*
+    Each timetable schedule contains:
+    1. Timetable location: TimetableLocation;
+
+    - Timetable location contains the following params in order:
+    `id: string`, `name: string`
+    e.g new TimetableLocation('0', 'Room 1')
+
+
+    2. Array of Timetable events: Array<TimetableEvent>
+
+    - Timetable event contains the following params in order:
+    `id: string`, `startTime: Date`, `endTime: Date`, `title: string`, `(optional) details: string`, `(optional) color: string`
+    */
+
+    this.schedules = [
+      new TimetableSchedule(
+        new TimetableLocation('0', 'Room 1'),
+        /* Timetable location contains the following params in order: `id: string`, `name: string`*/
+        [
+          new TimetableEvent('01',
+            new Date(new Date().setHours(8, 0, 0)),
+            new Date(new Date().setHours(8, 30, 0)), 'Event 1', 'Staff meeting', '#ff0000')
+        ]
+      ),
+
+      new TimetableSchedule(
+        new TimetableLocation('1', 'Room 2'),
+        [
+          new TimetableEvent('02',
+            new Date(new Date().setHours(9, 0, 0)),
+            new Date(new Date().setHours(9, 30, 0)), 'Event 2', 'Student meeting')
+        ]
+      )
+    ];
+  }
+
+  eventClicked(event: TimetableEvent): void {
+    window.alert(event.title);
   }
 
 }
