27d6291a367727695cd18fe53b6647e9fd646b8f
Ana Bikic <57949805+IT180175@users.noreply.github.com>
2022-10-09 13:30:54 +0200

    Reactive-Forms
    Reactive Forms in Angular erstellt

--- Reservierungsystem/reservation-system/src/app/site-components/reservation-page/reservation-page.component.ts
+++ Reservierungsystem/reservation-system/src/app/site-components/reservation-page/reservation-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
 
 
 @Component({
@@ -9,11 +9,24 @@ import { FormControl } from '@angular/forms';
 })
 export class ReservationPageComponent implements OnInit {
 
-  name = new FormControl('');
+  title = 'ReactiveForms';
+  reactiveForm: FormGroup;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+
+    this.reactiveForm = new FormGroup({
+      nachname: new FormControl(null),
+      telefonnummer: new FormControl(null),
+      tischnummer: new FormControl('2'),
+      datum: new FormControl(null),
+      personenanzahl: new FormControl('2'),
+      zeit: new FormControl(null)
+
+
+    });
+
   }
 
 }
