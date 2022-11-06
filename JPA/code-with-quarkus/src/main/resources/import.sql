e695be575d5cfa0089c61ea7cd29c4e8ab505f71
David Ursprung <ursprungdavid@gmail.com>
2022-11-06 15:49:48 +0100

    import.sql geupdated

--- JPA/code-with-quarkus/src/main/resources/import.sql
+++ JPA/code-with-quarkus/src/main/resources/import.sql
@@ -1,11 +1,51 @@
-insert into Job(job_title, permission) values('Chef', true);
+insert into Job(job_title, permission)
+values('Chef', true),
+      ('Koch', false),
+      ('Kellner', false);
 
-insert into Employee(username, first_name, last_name, password, job_id) values('JosefC', 'Josef', 'Cagitz', 'password123', 1);
+insert into Employee(username, first_name, last_name, password, job_id)
+values('JosefC', 'Josef', 'Cagitz', 'password123', 1),
+      ('NicoleC', 'Nicole', 'Cagitz', 'password123', 1),
+      ('AnnikaC', 'Annika', 'Cagitz', 'password123', 2),
+      ('KatharinaR', 'Katharina', 'Ransmair', 'password123', 3);
 
-insert into Customer(first_name, last_name, email, password, telephoneNumber) values('Fabian', 'Maar', 'fabian@maar.com', '123', '123456789');
+insert into Customer(first_name, last_name, email, password, telephoneNumber)
+values('Fabian', 'Maar', 'fabian@maar.com', '123', '123456789'),
+      ('David', 'Ursprung', 'david@ursprung.com', '123', '123456789'),
+      ('Ana', 'Bikic', 'ana@bikic.com', '123', '123456789'),
+      ('Hans Christian', 'Hammer', 'fabian@hammer.com', '123', '123456789'),
+      ('Christian', 'Aberger', 'christian@aberger.com', '123', '123456789');
 
-insert into Room(room_name, summer_winter) values('Raum 1', 'Winter');
+insert into Room(room_name, summer_winter)
+values('Raum 1', 'Winter'),
+      ('Raum 2', 'Winter'),
+      ('Raum 3', 'Winter'),
+      ('Raum 4', 'Winter'),
+      ('Saal 1', 'Winter'),
+      ('Saal 2', 'Winter'),
+      ('Terrasse', 'Sommer'),
+      ('Gastgarten', 'Sommer');
 
-insert into TableEntity(seats, room_id) values(4, 1);
+insert into TableEntity(seats, room_id)
+values(6, 1),
+      (4, 1),
+      (4, 1),
+      (4, 1),
+      (4, 2),
+      (2, 2),
+      (4, 2),
+      (6, 3),
+      (6, 3),
+      (6, 3),
+      (4, 3),
+      (4, 3),
+      (4, 3),
+      (4, 3),
+      (5, 3);
 
-insert into Reservation(employee_id, customer_id, customer_name, reservation_time, reservation_date, reservation_timestamp, person_amount, tableno) values(1, 1, 'Maar', 1, '10.10.2022', current_timestamp, 4, 1);
+insert into Reservation(employee_id, customer_id, customer_name, reservation_time, reservation_date, reservation_timestamp, person_amount, tableno)
+values(1, 1, 'Maar', 1, '06.11.2022', current_timestamp, 6, 1),
+      (1, 2, 'Ursprung', 1, '06.11.2022', current_timestamp, 3, 2),
+      (2, 3, 'Bikic', 1, '06.11.2022', current_timestamp, 6, 8),
+      (1, 4, 'Hammer', 1, '06.11.2022', current_timestamp, 5, 10),
+      (2, 5, 'Aberger', 1, '06.11.2022', current_timestamp, 4, 5);
