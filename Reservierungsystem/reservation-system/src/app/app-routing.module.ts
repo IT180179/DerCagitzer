4c88bc71a329cb82a22459443a41340520c81e19
Annika Cagitz <annika.cagitzz@gmail.com>
2022-11-26 21:27:13 +0100

    commit

--- Reservierungsystem/reservation-system/src/app/app-routing.module.ts
+++ Reservierungsystem/reservation-system/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+
 import {DayviewPageComponent} from "./site-components/dayview-page/dayview-page.component";
 import {ReservationPageComponent} from "./site-components/reservation-page/reservation-page.component";
 
 const routes: Routes = [
-  {path: '', component: DayviewPageComponent},
-  {path: 'reservation', component: ReservationPageComponent},
+  { path: '', component: DayviewPageComponent },
+  { path: 'reservation', component: ReservationPageComponent },
+  { path: '', redirectTo: '/', pathMatch: 'full'}
 ];
 
 @NgModule({
--- Reservierungsystem/reservation-system/src/app/site-components/reservation-page/reservation-page.component.ts
+++ Reservierungsystem/reservation-system/src/app/site-components/reservation-page/reservation-page.component.ts
@@ -2,7 +2,7 @@ import {Component, EventEmitter, Inject, Input, OnInit, Output} from '@angular/c
 import {UntypedFormBuilder, UntypedFormControl, UntypedFormGroup} from '@angular/forms';
 import {Reservation} from "../../shared/reservation";
 import {MAT_DIALOG_DATA} from "@angular/material/dialog";
-
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-reservation-page',
@@ -18,7 +18,7 @@ export class ReservationPageComponent implements OnInit {
   today = new Date();
   test: Date;
 
-  constructor(private fb: UntypedFormBuilder, @Inject(MAT_DIALOG_DATA) public data: any) { }
+  constructor(public http: HttpClient, private fb: UntypedFormBuilder, @Inject(MAT_DIALOG_DATA) public data: any) { }
 
 
   ngOnInit(){
@@ -26,7 +26,7 @@ export class ReservationPageComponent implements OnInit {
 
     console.log(this.data)
 
-    this.reactiveForm = new UntypedFormGroup({
+    this.addressForm = new UntypedFormGroup({
       nachname: new UntypedFormControl(null),
       telefonnummer: new UntypedFormControl(null),
       tischnummer: new UntypedFormControl(this.data.tablenr),
@@ -40,13 +40,38 @@ export class ReservationPageComponent implements OnInit {
     });
 
   }
-
-  submitForm() {
-    const formValue = this.reactiveForm.value;
+newdata: any
+  addressForm: any;
+  onSubmit(data: any) {
+    const formValue = this.addressForm.value;
     const newReservation: Reservation = {
       ...formValue,
     }
     this.submitReservation.emit(newReservation);
+
+    this.newdata = {
+      customer:{
+        last_name: this.addressForm.nachname,
+        telNr: this.addressForm.telefonnummer,
+        email: this.addressForm.email,
+        first_name: this.addressForm.vorname,
+      },
+      customer_name: this.addressForm.nachname,
+      start_time: this.addressForm.startzeit,
+      end_time: this.addressForm.endzeit,
+      reservation_date: this.addressForm.datum,
+      person_amount: this.addressForm.personenanzahl,
+      tableEntity: {
+        tableno: this.addressForm.tischnummer
+      }
+    };
+
+    console.warn(this.newdata)
+
+    this.http.post('http://localhost:8080/reservation/add', this.newdata)
+      .subscribe((result)=>{
+        console.log(result)
+      });
 
     this.reactiveForm.reset();
   }
