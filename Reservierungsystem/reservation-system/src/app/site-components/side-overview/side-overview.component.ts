38b4a7868a9dd33b9c4e9040b56697aa227d1e89
David Ursprung <ursprungdavid@gmail.com>
2022-12-04 17:47:58 +0100

    tagesauslasung

--- Reservierungsystem/reservation-system/src/app/site-components/side-overview/side-overview.component.ts
+++ Reservierungsystem/reservation-system/src/app/site-components/side-overview/side-overview.component.ts
@@ -10,11 +10,41 @@ import {ReservationService} from "../../shared/reservation.service";
 export class SideOverviewComponent implements OnInit {
 
   @Input() infoReservation: Reservation;
-  @Input() reservationsPerDay: number;
+  @Input() resultPerDay: number;
+  @Input() resultPerRoomNoon: number;
+  @Input() resultPerRoomEvening: number;
+  reservationsPerDay: number;
+  reservationsPerRoomNoon: number;
+  reservationsPerRoomEvening: number;
 
-  constructor() {}
+  constructor(private rs: ReservationService) {}
 
   ngOnInit(): void {
+    this.reservationsPerDayCalc("01.12.2022");
   }
 
+  reservationsPerDayCalc(date: String) {
+    this.rs.countReservationsPerDay(date).subscribe(
+      (r: number) => {
+        this.reservationsPerDay = r;
+        this.resultPerDay = (this.reservationsPerDay / 124) * 100;
+      }
+    )
+  }
+  reservationsPerRoomNoonCalc(date: String, room: number) {
+    this.rs.countReservationsPerRoomNoon(date, room).subscribe(
+      (r: number) => {
+        this.reservationsPerRoomNoon = r;
+        this.resultPerRoomNoon = (this.reservationsPerRoomNoon / 31) * 100;
+      }
+    )
+  }
+  reservationsPerRoomEveningCalc(date: String, room: number) {
+    this.rs.countReservationsPerRoomEvening(date, room).subscribe(
+      (r: number) => {
+        this.reservationsPerRoomEvening = r;
+        this.resultPerRoomEvening = (this.reservationsPerRoomEvening / 31) * 100;
+      }
+    )
+  }
 }
