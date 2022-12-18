064c9ed8178f7e8d3ce92199439328a3ac7f381d
Annika Cagitz <annika.cagitzz@gmail.com>
2022-12-18 14:17:14 +0100

    form changes

--- Reservierungsystem/reservation-system/src/app/site-components/reservation-page/reservation-page.component.ts
+++ Reservierungsystem/reservation-system/src/app/site-components/reservation-page/reservation-page.component.ts
@@ -13,6 +13,8 @@ import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {HttpClient} from "@angular/common/http";
 import {ActivatedRoute, Router} from "@angular/router";
 import {Dialog} from "../dayview-page/dayview-page.component";
+import {Time} from "@angular/common";
+import {Runtime} from "inspector";
 
 @Component({
   selector: 'app-reservation-page',
@@ -32,17 +34,19 @@ export class ReservationPageComponent implements OnInit {
 
 
   constructor(private fb: FormBuilder,
-              private errorHandler:ErrorHandler,
+              private errorHandler: ErrorHandler,
               public dialogRef: MatDialogRef<Dialog>,
               public http: HttpClient,
-              @Inject(MAT_DIALOG_DATA) public data: any,private route: ActivatedRoute,
+              @Inject(MAT_DIALOG_DATA) public data: any, private route: ActivatedRoute,
               private router: Router
-  ) { }
+  ) {
+  }
 
 
-  ngOnInit(): void{
 
+  ngOnInit(): void {
 
+    this.today = new Date()
     this.test = this.data.date.toLocaleDateString()
 
     console.log(this.test)
@@ -54,7 +58,7 @@ export class ReservationPageComponent implements OnInit {
       tischnummer: new UntypedFormControl(this.data.tablenr, Validators.max(3)),
       startzeit: new UntypedFormControl(this.data.starttime),
       endzeit: new UntypedFormControl(this.data.endtime),
-      datum: new UntypedFormControl(this.test),
+      datum: new UntypedFormControl(this.data.date),
       personenanzahl: new UntypedFormControl('2'),
       vorname: new UntypedFormControl(null),
       email: new UntypedFormControl(null, Validators.email),
@@ -63,13 +67,14 @@ export class ReservationPageComponent implements OnInit {
 
     console.log(this.addressForm.get('datum').value)
 
-
-    this.addressForm.patchValue({datum: this.test})
+    this.addressForm.patchValue({datum: this.data.date})
     this.addressForm.patchValue({tischnummer: this.data.tablenr})
 
 
   }
+
   newdata: any
+
   onSubmit(data: any) {
     const formValue = this.addressForm.value;
     const newReservation: Reservation = {
@@ -94,7 +99,7 @@ export class ReservationPageComponent implements OnInit {
 
     console.log(this.newdata)
     this.http.post('http://localhost:8080/reservation/add', this.newdata)
-      .subscribe((result)=>{
+      .subscribe((result) => {
         this.router.navigate(['..'], {relativeTo: this.route})
         console.log(result)
       });
@@ -109,11 +114,20 @@ export class ReservationPageComponent implements OnInit {
     const day = d.getDay();
 
     /* Prevent Monday and Tueasday for select. */
-    return day !== 1 && day !== 2 ;
+    return day !== 1 && day !== 2;
   }
+  selectedOption: any;
 
   onNoClick(): void {
 
     this.dialogRef.close();
+  }
+
+  setEndTime(selectedOption: Time) {
+    console.log(selectedOption)
+    //console.log( String( selectedOption.hours+  2 * 60 * 60 * 1000))
+    document.getElementById("timeinput").innerText = String(selectedOption);
+
+
   }
 }
