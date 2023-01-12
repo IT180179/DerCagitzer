690fe5cdb05e4df504a9c1d1bbc5d197f16b837b
IT180205 <56590851+IT180205@users.noreply.github.com>
2023-01-12 18:11:18 +0100

    3 Calendar

--- Reservierungsystem/reservation-system/src/app/site-components/calendar/calendar.component.ts
+++ Reservierungsystem/reservation-system/src/app/site-components/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, EventEmitter, OnInit, Output, Renderer2, ViewChild} from '@angular/core';
 import {MatCalendar} from "@angular/material/datepicker";
 
 
@@ -7,7 +7,7 @@ import {MatCalendar} from "@angular/material/datepicker";
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
 })
-export class CalendarComponent implements OnInit {
+export class CalendarComponent implements AfterViewInit {
 
   @ViewChild('calendar', { static: true })
   calendar: MatCalendar<Date>;
@@ -17,6 +17,13 @@ export class CalendarComponent implements OnInit {
 
 
   firstDate = new Date();
+  start0 = new Date()
+  start1 = new Date()
+
+  start2 = new Date(this.start0.setMonth(this.firstDate.getMonth()+1));
+  start3 = new Date(this.start1.setMonth(this.firstDate.getMonth()+2));
+  secondDate?: Date
+  thirdDate?: Date
 
   //secondDate = new Date();
 //  month2 = this.firstDate.getMonth()+1;
@@ -35,20 +42,46 @@ export class CalendarComponent implements OnInit {
     return day !== 1 && day !== 2 ;
   }
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    const buttons = document
+      .querySelectorAll('.mat-calendar-previous-button, .mat-calendar-next-button');
 
+    if (buttons) {
+      Array.from(buttons).forEach(button => {
+        this.renderer.listen(button, 'click', () => {
+          console.log(button)
+
+          //this.calendar.activeDate.setMonth(this.calendar.activeDate.getMonth()+1);
+        });
+      });
+    }
 
   }
 
-  dateChanged() {
-
-      this.calendar.activeDate = this.firstDate;
-      this.dateSelected.emit(this.firstDate);
-
-      console.log(this.firstDate)
 
 
+  dateChanged(dateString: String) {
+      if(dateString == 'date1'){
+        this.calendar.activeDate = this.firstDate;
+        this.secondDate = undefined
+        this.thirdDate = undefined
+        this.dateSelected.emit(this.firstDate);
+      }
+      if(dateString == 'date2'){
+      this.calendar.activeDate = this.secondDate;
+      this.firstDate = undefined
+      this.thirdDate = undefined
+      this.dateSelected.emit(this.secondDate);
+     }
+      if(dateString == 'date3'){
+      this.calendar.activeDate = this.thirdDate;
+      this.firstDate = undefined
+      this.secondDate = undefined
+      this.dateSelected.emit(this.thirdDate);
+     }
   }
+
+
 }
