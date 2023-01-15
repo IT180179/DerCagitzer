dcde066ebe0a56f51997d9f6bf5511f444f745b4
IT180205 <56590851+IT180205@users.noreply.github.com>
2023-01-15 23:33:51 +0100

    Reservation

--- Reservierungsystem/reservation-system/src/app/site-components/reservation-page/reservation-page.component.ts
+++ Reservierungsystem/reservation-system/src/app/site-components/reservation-page/reservation-page.component.ts
@@ -1,11 +1,12 @@
 import {Component, ErrorHandler, EventEmitter, Inject, Input, OnInit, Output} from '@angular/core';
 import {
+  AbstractControl,
   Form,
   FormBuilder,
   FormGroup,
   UntypedFormBuilder,
   UntypedFormControl,
-  UntypedFormGroup,
+  UntypedFormGroup, ValidationErrors, ValidatorFn,
   Validators
 } from '@angular/forms';
 import {Reservation} from "../../shared/reservation";
@@ -14,6 +15,7 @@ import {HttpClient} from "@angular/common/http";
 import {ActivatedRoute, Router} from "@angular/router";
 import {Dialog} from "../dayview-page/dayview-page.component";
 import {DatePipe, Time} from "@angular/common";
+import {distinctUntilChanged} from "rxjs";
 
 @Component({
   selector: 'app-reservation-page',
@@ -24,7 +26,7 @@ export class ReservationPageComponent implements OnInit {
 
   addressForm: FormGroup;
   errors: any = {};
-
+  checkTime: boolean = true;
 
   @Input() reservation?: Reservation;
   @Output() submitReservation = new EventEmitter<Reservation>();
@@ -54,12 +56,13 @@ export class ReservationPageComponent implements OnInit {
 
     console.log(this.test)
 
+
     this.addressForm = new UntypedFormGroup({
       name: new UntypedFormControl(null,
         [Validators.required, Validators.minLength(2)]),
       telefonnummer: new UntypedFormControl(null, [Validators.required, Validators.minLength(8)]),
       tischnummer: new UntypedFormControl(this.data.tablenr, Validators.required),
-      startzeit: new UntypedFormControl(this.data.starttime),
+      startzeit: new UntypedFormControl(this.data.starttime ),
       endzeit: new UntypedFormControl(this.data.endtime),
       datum: new UntypedFormControl(this.data.date),
       personenanzahl: new UntypedFormControl('2'),
@@ -68,17 +71,48 @@ export class ReservationPageComponent implements OnInit {
       anmerkungen: new UntypedFormControl(null)
     });
 
-    console.log(this.data.tablenr)
 
 
+    this.addressForm.get('startzeit').valueChanges.subscribe(value => {
 
-    console.log(this.addressForm.get('datum').value)
+
+      var checkStart = this.formatTime(value, "h:m");
+      console.log(checkStart)
+      var checkEnd = this.formatTime(this.addressForm.get('endzeit').value,"h:m");
+      if (checkStart >= checkEnd){
+        this.checkTime = false
+      }else {
+       this.checkTime = true
+      }
+        console.log(this.checkTime);
+    }
+    )
 
     this.addressForm.patchValue({datum: this.data.date})
     this.addressForm.patchValue({tischnummer: this.data.tablenr})
 
 
   }
+
+  validateTime(control: AbstractControl){
+    var checkStart = this.formatTime(control.value('startzeit'), "h:m");
+    var checkEnd = this.formatTime(control.value('endzeit'),"h:m");
+    if (checkStart >= checkEnd){
+      return {invalidTime: true}
+    }else {
+      return null
+    }
+
+  }
+
+  formatTime(dStr, format){
+    var now = new Date();
+    now.setHours(dStr.substr(0,dStr.indexOf(":")));
+    now.setMinutes(dStr.substr(dStr.indexOf(":")+1));
+    now.setSeconds(0);
+    return now;
+  }
+
 
   newdata: any
 
